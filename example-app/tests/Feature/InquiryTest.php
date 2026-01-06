e5f4fdc5a44654b89e701ec375d0f3fd3a14d9d0
ev-support3-max <ev-support3@miraiz-enechange.co.jp>
2026-01-06 02:28:28 +0000

    test add

--- example-app/tests/Feature/InquiryTest.php
+++ example-app/tests/Feature/InquiryTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Inquiry;
+use App\Models\User;
 
 class InquiryTest extends TestCase
 {
@@ -31,4 +32,55 @@ class InquiryTest extends TestCase
             ]);
         }
     }
+
+    public function test_inquiry_validation_errors()
+    {
+        $response = $this->post(route('contact.store'), [
+            'first_name' => '山田',
+            'last_name' => '太郎',
+            'corp_name' => '例株式会社',
+            'email' => 'test',
+        ]);
+
+        $response->assertSessionHasErrors(['email']);
+        $this->assertDatabaseCount('inquiries', 0);
+    }
+
+    public function test_inquiry_fails_when_name_is_long()
+    {
+        $response = $this->post(route('contact.store'),[
+            'first_name' => str_repeat('a', 300),
+            'last_name' => str_repeat('b', 300),
+            'email' => 'test@example.com',
+            'content' => 'This is a test inquiry.',
+        ]);
+
+        $response->assertSessionHasErrors(['first_name', 'last_name']);
+    }
+
+    public function test_gest_connot_access_admin()
+    {
+        $response = $this->get('/admin/inquiries');
+        $response->assertRedirect('/admin/login');
+    }
+
+    // public function test_admin_can_access_inquiries()
+    // {
+    //     $user = User::factory()->create();
+
+    //     $response = $this->actingAs($user)->get('/admin/inquiries');
+    //     $response->assertStatus(200);
+    // }
+
+    // public function test_api_create_inquiry()
+    // {
+    //     $response = $this->postJson('/api/inquiries', [
+    //         'first_name' => 'Yamada',
+    //         'last_name' => 'Taro',
+    //         'corp_name' => 'Example Corp',
+    //         'email' => 'yamada@example.com',
+    //     ]);
+    //     $response->assertStatus(201)
+    //              ->assertJson(['message' => 'Inquiry created API successfully.']);
+    // }
 }
